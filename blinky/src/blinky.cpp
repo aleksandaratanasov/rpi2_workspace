a99ed54d8f4c951a81580d0b6fd2ac09f8b9cd38
aleksandaratanasov <redbaronqueen@gmail.com>
2015-08-27 19:48:30 +0000

    Changed blinky to non-root usable state using 'gpio export'

--- blinky/src/blinky.cpp
+++ blinky/src/blinky.cpp
@@ -4,21 +4,22 @@
 
 void setLED(const std_msgs::Bool::ConstPtr& msg)
 {
-  ROS_INFO_STREAM("Turning LED on GPIO 17 " << msg->data ? "on" : "off");
+  ROS_INFO_STREAM("Turning LED " << (msg->data ? "on" : "off"));
 
-  if(msg->data) digitalWrite(17, HIGH);
-  else digitalWrite(17, LOW);
+  if(msg->data) digitalWrite(19, HIGH);
+  else digitalWrite(19, LOW);
 }
 
 int main(int argc, char **argv)
 {
+  ROS_INFO("Run 'gpio export 19 out' before starting this node!");
   ros::init(argc, argv, "blinky");
   ros::NodeHandle n;
-  wiringPiSetupGpio();
-  ros::Subscriber sub = n.subscribe("led17", 10, setLED);
 
-  // Configure GPIO17
-  pinMode(17, OUTPUT);
+  // The gpio 19 is configured outside using the 'gpio export 19 out' command
+  wiringPiSetupSys(); // When exporting pins this setup is used
+
+  ros::Subscriber sub = n.subscribe("led", 10, setLED);
 
   ros::spin();
 
